d0a702caab72fb18436ddf28c89758ad3bac906e
Raymond Perminov <raymondproduction@ukr.net>
2017-04-16 20:28:57 +0000

    Counts

--- test2.js
+++ test2.js
@@ -10,14 +10,14 @@ var pins = [7, 11, 12, 13, 15, 16, 18, 22];
 var digits = [
   [7, 13, 11, 16, 15, 22],
   [15, 16],
-  [22,15,18,13,11],
-  [22,15,18,16,11],
-  [7,18,15,16],
-  [22,7,18,16,11],
-  [22,7,13,18,11,16],
-  [22,15,16],
-  [22,7,15,18,13,16,11],
-  [22,15,16,18,7,11]
+  [22, 15, 18, 13, 11],
+  [22, 15, 18, 16, 11],
+  [7, 18, 15, 16],
+  [22, 7, 18, 16, 11],
+  [22, 7, 13, 18, 11, 16],
+  [22, 15, 16],
+  [22, 7, 15, 18, 13, 16, 11],
+  [22, 15, 16, 18, 7, 11]
 ]
 
 
@@ -26,21 +26,31 @@ var delay = 200;
 var count = 0;
 var max = 8;
 
-gpio.setup(swichPins, gpio.DIR_OUT, function() {
-  gpio.write(swichPins, 0);
-});
 
-digits[7].forEach(function(pin) {
-  console.log(pin);
-  gpio.setup(pin, gpio.DIR_OUT, function() {
-    gpio.write(pin, 1);
-  });
-});
 
 gpio.setup(16, gpio.DIR_OUT, pause);
 
+function showDigit(n) {
+  gpio.setup(swichPins, gpio.DIR_OUT, function() {
+    gpio.write(swichPins, 0);
+
+    digits[n].forEach(function(pin, i, arr) {
+      console.log(pin);
+      gpio.setup(pin, gpio.DIR_OUT, function() {
+        if (i == arr.length - 1) {
+          gpio.write(pin, 1, pause);
+          console.log('ok');
+        } else {
+          gpio.write(pin, 1);
+        }
+      });
+    });
+  });
+}
+
+
 function pause() {
-  setTimeout(closePins, 2000);
+  setTimeout(closePins, 1000);
 }
 
 function closePins() {
@@ -48,6 +58,20 @@ function closePins() {
     console.log('All pins unexported');
   });
 }
+
+
+var intervalID = setInterval(function() {
+  console.log(count);
+  showDigit(count);
+  count++;
+  if (count > 9) {
+    console.log('End!');
+    clearInterval(intervalID);
+  }
+}, 2000);
+
+
+
 /*
 
   22
